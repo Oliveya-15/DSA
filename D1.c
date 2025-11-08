07c2828d2a597a30cee790bde25b624635a7d405
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-11-08 22:39:08 +0530

    D1 : DSA : Basic Patterns

--- D1.c
+++ D1.c
@@ -1,30 +1,32 @@
-//STARTING WITH DSA BASICS :-
-// PATTERNS :-
+// STARTING WITH DSA BASICS :-
+//  PATTERNS :-
 
 /* Patter 1:-
 1234
 1234
 1234 */
 #include <stdio.h>
-int main() {
+int main()
+{
     int n;
     printf("Enter the range: ");
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            printf("%d",j);
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            printf("%d", j);
         }
         printf("\n");
     }
     return 0;
-} 
-
+}
 
 /* Patter 1:-
 ****
 ****
 ****
-****  
+****
 
 #include <stdio.h>
 int main() {
@@ -39,7 +41,6 @@ int main() {
     }
     return 0;
 }    */
-
 
 /* Patter 3:-
 ABCD
@@ -63,8 +64,6 @@ int main() {
     return 0;
 }    */
 
-
-
 /* Patter 4:-
 123
 456
@@ -86,7 +85,6 @@ int main() {
     return 0;
 }    */
 
-
 /* Patter 4:-
 *
 **
@@ -106,7 +104,6 @@ int main() {
     }
     return 0;
 }    */
-
 
 /* Patter 4:-
 1
@@ -128,7 +125,6 @@ int main() {
     return 0;
 }    */
 
-
 /* Patter 4:-
 1
 12
@@ -149,7 +145,6 @@ int main() {
     return 0;
 }    */
 
-
 /* Patter 4:-
 1
 21
@@ -169,6 +164,126 @@ int main() {
     }
     return 0;
 }    */
+
+/* Patter 4:-
+1
+23
+456
+78910
+
+#include <stdio.h>
+int main() {
+    int num = 1;
+    int n;
+    printf("Enter the range: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            printf("%d",num);
+            num++;
+        }
+        printf("\n");
+    }
+    return 0;
+}    */
+
+/* Patter 4:-
+1111
+ 222
+  33
+   4
+
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the range: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            printf(" ");
+        }
+        for(int k=0;k<n-i;k++){
+        printf("%d",i+1);
+        }
+        printf("\n");
+    }
+    return 0;
+}    */
+
+/* Patter 4:-
+      1
+     121
+    12321
+   1234321
+
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the range: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            printf(" ");
+        }
+            for(int k=1;k<=i+1;k++){
+                printf("%d",k);
+            }
+                for(int m=i; m>=1;m--){
+                    printf("%d",m);
+                }
+        printf("\n");
+    }
+    return 0;
+}    */
+
+
+/* Patter 4:-
+      *
+    *   *
+   *     *
+  *       *
+   *     *
+    *  *
+     *
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    int i, j, k;               
+    printf("Enter the range: ");
+    if (scanf("%d", &n) != 1) return 0;
+
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n - i - 1; j++){
+            printf(" ");
+        }
+        printf("*");
+        if(i != 0){
+            for(j = 0; j < 2*i - 1; j++){
+                printf(" ");
+            }
+            printf("*");
+        }
+        printf("\n");          
+    }
+
+    for(i = 0; i < n - 1; i++){
+        for(j = 0; j < i + 1; j++){
+            printf(" ");
+        }
+        printf("*");
+        if (i != n - 2){
+            for(k = 0; k < 2*(n - i) - 5; k++){
+                printf(" ");
+            }
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+    */
 
 
 
